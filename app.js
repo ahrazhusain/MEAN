c0d96f45406947b8b11bf06b06ee12044d5e950c
ahrazhusain <ahraz.husain@hotmail.com>
2015-10-30 00:21:35 -0400

    Cleaning Up- 1

--- app.js
+++ app.js
@@ -9,62 +9,50 @@ var bodyParser = require('body-parser');
 var mongo = require('mongodb');
 var monk = require('monk');
 var db = monk('localhost:27017/nodetest1');
-
 var routes = require('./routes/index');
-
 var app = express();
-
-
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
-
 // Can uncomment after placing favicon in /public
 //app.use(favicon(__dirname + '/public/favicon.ico'));
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({
+                              extended: false
+                              }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
 // Make our db accessible to our router
-app.use(function(req,res,next){
-    req.db = db;
-    next();
-});
-
+app.use(function(req, res, next) {
+        req.db = db;
+        next();
+        });
 app.use('/', routes);
-
 /// catch 404 and forwarding to error handler
 app.use(function(req, res, next) {
-    var err = new Error('Not Found');
-    err.status = 404;
-    next(err);
-});
-
+        var err = new Error('Not Found');
+        err.status = 404;
+        next(err);
+        });
 /// error handlers===============================================
-
 // development error handler
 if (app.get('env') === 'development') {
     app.use(function(err, req, res, next) {
-        res.status(err.status || 500);
-        res.render('error', {
-            message: err.message,
-            error: err
-        });
-    });
+            res.status(err.status || 500);
+            res.render('error', {
+                       message: err.message,
+                       error: err
+                       });
+            });
 }
-
 // production error handler
 // no stacktraces leaked to user
 app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-        message: err.message,
-        error: {}
-    });
-});
-
-
+        res.status(err.status || 500);
+        res.render('error', {
+                   message: err.message,
+                   error: {}
+                   });
+        });
 module.exports = app;
-
